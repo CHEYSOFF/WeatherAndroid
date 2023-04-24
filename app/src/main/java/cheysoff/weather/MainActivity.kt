a92a476fdf9b59226e7ed42244dcdead58cb0c5d
CHEYSOFF <slobberyblanket@gmail.com>
2023-04-24 17:58:40 +0300

    It creates backthread

--- app/src/main/java/cheysoff/weather/MainActivity.kt
+++ app/src/main/java/cheysoff/weather/MainActivity.kt
@@ -1,46 +1,37 @@
 package cheysoff.weather
 
+
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.util.Log
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import cheysoff.weather.ui.theme.MyApplicationTheme
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import cheysoff.weather.parse.GetCoordinates
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : ComponentActivity() {
+
+    private val myScope = CoroutineScope(Dispatchers.Main) // create a new CoroutineScope
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("WOW", "it works no it doesnt")
         super.onCreate(savedInstanceState)
-        setContent {
-            MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+        Log.d("WOW", "it works no it doesnt")
+        val lifecycleScope = lifecycleScope
+        lifecycleScope.launch {
+            // Perform some long-running task
+            val response = GetCoordinates().coordinatesByCityName("Moscow")
+            // Update the UI on the main thread
+            withContext(Dispatchers.Main) {
+//                updateUi()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
     }
 }
