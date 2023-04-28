61a1fee232da394b01d66171806cfcc17c0657d7
CHEYSOFF <slobberyblanket@gmail.com>
2023-04-29 00:01:53 +0300

    minor UI changes

--- app/src/main/java/cheysoff/weather/MainActivity.kt
+++ app/src/main/java/cheysoff/weather/MainActivity.kt
@@ -1,16 +1,35 @@
 package cheysoff.weather
 
 import android.os.Bundle
+import android.text.style.BackgroundColorSpan
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Transparent
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.lifecycleScope
 import cheysoff.weather.parse.GetCoordinates
 import cheysoff.weather.parse.GetWeather
+import cheysoff.weather.ui.theme.LightPurple
+import cheysoff.weather.ui.theme.MyTransparent
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -45,13 +64,56 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Composable
 fun WeatherList(weatherList: List<Pair<Double, Double>>) {
-    Column {
-        for (i in weatherList.indices) {
-            val pair = weatherList.get(i)
-            Text(text = "(${pair.first}, ${pair.second})")
+    Image(
+        painter = painterResource(id = R.drawable.main_background),
+        contentDescription = "background image",
+        modifier = Modifier
+            .fillMaxSize()
+            .alpha(0.5f),
+        contentScale = ContentScale.FillBounds
+    )
+    Card(
+
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp)
+            .background(LightPurple),
+        shape = RoundedCornerShape(15.dp),
+
+
+        ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MyTransparent),
+
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                modifier = Modifier
+                    .background(MyTransparent),
+            ) {
+                for (i in weatherList.indices) {
+                    val pair = weatherList.get(i)
+                    Row(
+                        modifier = Modifier
+                            .background(MyTransparent)
+                    ) {
+                        Text(text = "(${pair.first}, ${pair.second})")
+                    }
+
+                }
+            }
+
         }
-//        weatherList.forEach { pair ->
+    }
+//    Column {
+//        for (i in weatherList.indices) {
+//            val pair = weatherList.get(i)
 //            Text(text = "(${pair.first}, ${pair.second})")
 //        }
-    }
+////        weatherList.forEach { pair ->
+////            Text(text = "(${pair.first}, ${pair.second})")
+////        }
+//    }
 }
--- app/src/main/java/cheysoff/weather/ui/theme/Color.kt
+++ app/src/main/java/cheysoff/weather/ui/theme/Color.kt
@@ -2,10 +2,12 @@ package cheysoff.weather.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val PurpleGrey80 = Color(0xFF4E2A84)
+val Pink80 = Color(0xFFAD1457)
+val Purple80 = Color(0xFF8E24AA)
+val Purple40 = Color(0xFFBA68C8)
+val PurpleGrey40 = Color(0xFF9575CD)
+val Pink40 = Color(0xFFF06292)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val LightPurple = Color(0x79817DAF)
+val MyTransparent = Color(0xFFFFFF)
